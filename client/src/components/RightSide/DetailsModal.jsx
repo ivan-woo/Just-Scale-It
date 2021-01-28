ba9644cf7bb91e0fea883e6ca65b69ced925e659
tangtrent <tangtrent@gmail.com>
2021-01-27 21:35:41 -0800

    Making changes to details modal. Exit is not dynamically rendered to fit in the modal

--- client/src/components/RightSide/DetailsModal.jsx
+++ client/src/components/RightSide/DetailsModal.jsx
@@ -10,39 +10,41 @@ const DetailsModal = (props) => {
   return (
     <div className="detailsModalContainer">
       <div className="preScrim" onClick={handleDetailsModalView}></div>
-      <button onClick={handleDetailsModalView} className="detailsExit">X</button>
-      <div className="detailsModal">
-        <div className="modalHeader">
-          <div className="detailsModalImage">
-            <img className="detailsImage" src={currShoe.media[0]} alt="image here"></img>
+      <div className="detailsAnimation">
+        <div className="detailsModal">
+          <button onClick={handleDetailsModalView} className="detailsExit">X</button>
+          <div className="modalHeader">
+            <div className="detailsModalImage">
+              <img className="detailsImage" src={currShoe.media[0]} alt="image here"></img>
+            </div>
+            <div className="detailsModalTitle">
+              <h1 className="detailsShoeName">LeBron 18</h1>
+              <div className="detailsShoePrice">$200</div>
+            </div>
           </div>
-          <div className="detailsModalTitle">
-            <h1 className="detailsShoeName">LeBron 18</h1>
-            <div className="detailsShoePrice">$200</div>
+          <div className="detailsModalInfo">
+            {splitBody.map((section, i) => {
+              if (i % 2 !== 0) {
+                return (
+                  <div key={i} className="headlineContainer">
+                    <p className="regularHeadline">{section}</p>
+                    <br></br>
+                  </div>
+                )
+              } else {
+                return (
+                  <p key={i} className="headline">{section}</p>
+                )
+              }
+            })}
+            <ul className="moreBenefits">
+            {moreBenefits.map(benefit => {
+              return (
+                <li>{benefit}</li>
+              )
+            })}
+            </ul>
           </div>
-        </div>
-        <div className="detailsModalInfo">
-          {splitBody.map((section, i) => {
-            if (i % 2 !== 0) {
-              return (
-                <div key={i} className="headlineContainer">
-                  <p className="regularHeadline">{section}</p>
-                  <br></br>
-                </div>
-              )
-            } else {
-              return (
-                <p key={i} className="headline">{section}</p>
-              )
-            }
-          })}
-          <ul className="moreBenefits">
-          {moreBenefits.map(benefit => {
-            return (
-              <li>{benefit}</li>
-            )
-          })}
-          </ul>
         </div>
       </div>
     </div>
